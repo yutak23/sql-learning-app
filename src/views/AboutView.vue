f2d8d4fa2dcc40b3f33335da0b89797d804508b7
yutak23 <yuta.katayama.23@gmail.com>
2024-08-25 16:11:11 +0900

    chore: add hono for pages functions

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,26 @@
 <template>
 	<div class="about">
 		<h1>This is an about page</h1>
-		<v-chip color="primary"> Primary </v-chip>
+		<v-chip color="primary" @click="clickFetch"> Primary! </v-chip>
 	</div>
 </template>
+
+<script>
+export default {
+	name: 'AboutView',
+	methods: {
+		async clickFetch() {
+			try {
+				const response = await fetch('/api/hono');
+				const data = await response.json();
+				console.log(data);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	}
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
--- vite.config.js
+++ vite.config.js
@@ -10,24 +10,67 @@ import Components from 'unplugin-vue-components/vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import eslint from 'vite-plugin-eslint';
 
+// for hono
+import build from '@hono/vite-cloudflare-pages';
+import devServer from '@hono/vite-dev-server';
+// eslint-disable-next-line import/no-unresolved
+import adapter from '@hono/vite-dev-server/cloudflare';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		nodePolyfills({ protocolImports: true }),
-		vue({ template: { transformAssetUrls } }),
-		vuetify({
-			autoImport: true,
-			styles: { configFile: 'src/styles/settings.scss' }
-		}),
-		// components auto importing for Vue(No more manual importing of components)
-		Components(),
-		vueDevTools(),
-		eslint()
-	],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+	// for hono
+	if (mode === 'server')
+		return {
+			plugins: [
+				build({ entry: 'srv/index.js' }),
+				devServer({
+					adapter,
+					entry: 'srv/index.js'
+					// exclude: ['node_modules', 'src']
+				})
+			],
+			// Configuration for avoiding the following errors
+			/**
+			 * Error: The following dependencies are imported but could not be resolved:
+			 * @/plugins (imported by /home/study/workspace/sql-learning-app/src/main.js)
+			 * Are they installed?
+			 */
+			resolve: {
+				alias: {
+					'@': fileURLToPath(new URL('./src', import.meta.url))
+				}
+			},
+			server: { host: '0.0.0.0', port: 3000, watch: 'srv/**' }
+		};
+
+	// for vue
+	return {
+		plugins: [
+			nodePolyfills({ protocolImports: true }),
+			vue({ template: { transformAssetUrls } }),
+			vuetify({
+				autoImport: true,
+				styles: { configFile: 'src/styles/settings.scss' }
+			}),
+			// components auto importing for Vue(No more manual importing of components)
+			Components(),
+			vueDevTools(),
+			eslint()
+		],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url))
+			}
+		},
+		server: {
+			proxy: {
+				'^/api': {
+					target: 'http://192.168.56.5:3000'
+				}
+			},
+			watch: 'src/**',
+			host: '0.0.0.0',
+			port: 8080
 		}
-	},
-	server: { host: '0.0.0.0', port: 8080 }
+	};
 });
