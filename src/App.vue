d2b6401c984357ed5a0d1f059c58e878491fa4ab
yutak23 <yuta.katayama.23@gmail.com>
2024-08-25 17:57:43 +0900

    feat: add app bar and basic navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,52 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-	<header>
-		<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+	<v-responsive class="border rounded">
+		<v-app>
+			<v-app-bar color="primary" :elevation="2">
+				<template v-slot:prepend>
+					<v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+				</template>
 
-		<div class="wrapper">
-			<HelloWorld msg="You did it!" />
+				<v-app-bar-title>App bar</v-app-bar-title>
+			</v-app-bar>
 
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/about">About</RouterLink>
-			</nav>
-		</div>
-	</header>
+			<v-navigation-drawer v-model="drawer" persistent>
+				<v-list :lines="false" nav>
+					<v-list-item
+						v-for="(menu, i) in menus"
+						:key="i"
+						:value="menu"
+						:to="menu.to"
+						exact
+						color="primary"
+					>
+						<template v-slot:prepend>
+							<v-icon :icon="menu.icon"></v-icon>
+						</template>
 
-	<RouterView />
+						<v-list-item-title>{{ menu.title }}</v-list-item-title>
+					</v-list-item>
+				</v-list>
+			</v-navigation-drawer>
+
+			<v-main>
+				<v-container>
+					<RouterView />
+				</v-container>
+			</v-main>
+		</v-app>
+	</v-responsive>
 </template>
 
-<style scoped>
-header {
-	line-height: 1.5;
-	max-height: 100vh;
-}
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
 
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
-nav {
-	width: 100%;
-	font-size: 12px;
-	text-align: center;
-	margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-	color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-	background-color: transparent;
-}
-
-nav a {
-	display: inline-block;
-	padding: 0 1rem;
-	border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-	border: 0;
-}
-
-@media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
-
-	nav {
-		text-align: left;
-		margin-left: -1rem;
-		font-size: 1rem;
-
-		padding: 1rem 0;
-		margin-top: 1rem;
-	}
-}
-</style>
+export default {
+	data: () => ({
+		drawer: false,
+		menus: [
+			{ title: 'Home', to: '/', icon: 'mdi-home' },
+			{ title: '問題一覧', to: '/problems', icon: 'mdi-file-document-multiple-outline' }
+		]
+	})
+};
+</script>
